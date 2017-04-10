8c630299566585b6f003fcc00444d24cd89bf158
Rafael Vareto <rafaelvareto@gmail.com>
2017-04-10 12:11:57 -0300

    Python: Changing FRGC script

---
+++ python/script_frgc_01.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#HOG    
+echo 'Main_OpenSet: HOG, Variable hashing, 10% Known size'
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d hog -r 5 -m 100 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d hog -r 5 -m 200 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d hog -r 5 -m 300 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d hog -r 5 -m 400 -ks 0.1
+
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d hog -r 5 -m 100 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d hog -r 5 -m 200 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d hog -r 5 -m 300 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d hog -r 5 -m 400 -ks 0.1
+
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d hog -r 5 -m 100 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d hog -r 5 -m 200 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d hog -r 5 -m 300 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d hog -r 5 -m 400 -ks 0.1
+
+
+#DF
+echo 'Main_OpenSet: DF, Variable hashing, 10% Known size'
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d df -r 2 -m 100 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d df -r 2 -m 200 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_1_label.txt -d df -r 2 -m 300 -ks 0.1
+
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d df -r 2 -m 100 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d df -r 2 -m 200 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_4_label.txt -d df -r 2 -m 300 -ks 0.1
+
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d df -r 2 -m 100 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d df -r 2 -m 200 -ks 0.1
+python main_openset.py -p ./frgcv1/ -f set_2_label.txt -d df -r 2 -m 300 -ks 0.1
