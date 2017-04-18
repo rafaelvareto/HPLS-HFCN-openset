df89aba5eac22d2bee5f16f385c07b0d804bc1d1
Rafael Vareto <rafaelvareto@gmail.com>
2017-04-18 17:55:52 -0300

    Python: Fixing vggface scripts

--- python/bash/script_vggface_df_05.sh
+++ python/bash/script_vggface_df_05.sh
@@ -2,14 +2,14 @@
 
 #DF
 echo '../main_openset: DF, Variable hashing, 50% Known size'
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 100 -ks 0.5
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 200 -ks 0.5
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 300 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 100 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 200 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 300 -ks 0.5
 
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 100 -ks 0.5
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 200 -ks 0.5
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 300 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 100 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 200 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 300 -ks 0.5
 
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 100 -ks 0.5
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 200 -ks 0.5
-python ../main_openset.py -p ../datasets/vggface/ -f vgg_set.txt -d df -r 2 -m 300 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 100 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 200 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d df -r 2 -m 300 -ks 0.5
---
+++ python/bash/script_vggface_hog_05.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#DF
+echo '../main_openset: DF, Variable hashing, 50% Known size'
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 100 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 200 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 300 -ks 0.5
+
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 100 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 200 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 300 -ks 0.5
+
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 100 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 200 -ks 0.5
+python ../main_openset.py -p ../datasets/vggface/files/ -f vgg_set.txt -d hog -r 2 -m 300 -ks 0.5
