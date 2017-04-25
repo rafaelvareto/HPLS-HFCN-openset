e77c974fd8b4ab3d0d9f20111a3607ae4bdae594
Rafael H. Vareto <rafaelvareto@gmail.com>
2017-04-24 23:23:55 -0300

    Python: Modifying feature bash files

--- python/bash/script_features_df_01.sh
+++ python/bash/script_features_df_01.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #DF
-echo '../main_openset_feat: DF, Variable hashing, 10% Known size'
-python ../main_openset_load.py -p ../features/ -f VGGFACE-15-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 100 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f VGGFACE-15-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 300 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f VGGFACE-15-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 500 -ks 0.1
+echo '../main_openset_load: DF, Variable hashing, 10% Known size'
+python ../main_openset_load.py -p ../features/ -f VGGFACE-15-DEEP-FEATURE-VECTORS.bin -r 10 -m 100 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f VGGFACE-15-DEEP-FEATURE-VECTORS.bin -r 10 -m 300 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f VGGFACE-15-DEEP-FEATURE-VECTORS.bin -r 10 -m 500 -ks 0.1
 
-python ../main_openset_load.py -p ../features/ -f PUPFIG83-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 100 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f PUPFIG83-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 300 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f PUPFIG83-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 500 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f PUPFIG83-DEEP-FEATURE-VECTORS.bin -r 10 -m 100 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f PUPFIG83-DEEP-FEATURE-VECTORS.bin -r 10 -m 300 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f PUPFIG83-DEEP-FEATURE-VECTORS.bin -r 10 -m 500 -ks 0.1
 
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-4-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 100 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-4-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 300 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-4-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 500 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-4-DEEP-FEATURE-VECTORS.bin -r 10 -m 100 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-4-DEEP-FEATURE-VECTORS.bin -r 10 -m 300 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-4-DEEP-FEATURE-VECTORS.bin -r 10 -m 500 -ks 0.1
 
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-1-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 100 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-1-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 300 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-1-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 500 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-1-DEEP-FEATURE-VECTORS.bin -r 10 -m 100 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-1-DEEP-FEATURE-VECTORS.bin -r 10 -m 300 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-1-DEEP-FEATURE-VECTORS.bin -r 10 -m 500 -ks 0.1
 
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-2-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 100 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-2-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 300 -ks 0.1
-python ../main_openset_load.py -p ../features/ -f FRGC-SET-2-DEEP-FEATURE-VECTORS.bin -d df -r 10 -m 500 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-2-DEEP-FEATURE-VECTORS.bin -r 10 -m 100 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-2-DEEP-FEATURE-VECTORS.bin -r 10 -m 300 -ks 0.1
+python ../main_openset_load.py -p ../features/ -f FRGC-SET-2-DEEP-FEATURE-VECTORS.bin -r 10 -m 500 -ks 0.1
