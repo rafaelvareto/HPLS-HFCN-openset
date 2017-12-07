749be5df989195df994ce0eb051fc8e7a17783b8
Rafael Vareto <rafaelvareto@gmail.com>
2017-12-07 18:13:18 -0200

    Trying to make it parallel - part II

--- python/HFCN_openset_load.py
+++ python/HFCN_openset_load.py
@@ -46,19 +46,18 @@ import keras.models
 def make_keras_picklable():
     def __getstate__(self):
         model_str = ""
-        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=False) as fd:
+        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=True) as fd:
             keras.models.save_model(self, fd.name, overwrite=True)
             model_str = fd.read()
         d = { 'model_str': model_str }
         return d
 
     def __setstate__(self, state):
-        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=False) as fd:
+        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=True) as fd:
             fd.write(state['model_str'])
             fd.flush()
             model = keras.models.load_model(fd.name)
         self.__dict__ = model.__dict__
-
 
     cls = keras.models.Model
     cls.__getstate__ = __getstate__
