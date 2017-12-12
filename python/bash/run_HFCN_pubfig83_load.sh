de7e4a4d382593b32d227b860dfa0f5733119f9d
Rafael Vareto <rafaelvareto@gmail.com>
2017-12-12 18:15:21 -0200

    Updating pubgif83 scripts

--- python/bash/run_HFCN_pubfig83_load.sh
+++ python/bash/run_HFCN_pubfig83_load.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 #DF
-echo 'PUPFIG83: DF, Variable hashing, 10% Known size, 90% Train/Test'
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 10 -ks 0.1 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 20 -ks 0.1 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 30 -ks 0.1 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 50 -ks 0.1 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 100 -ks 0.1 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 300 -ks 0.1 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 500 -ks 0.1 -ts 0.9
+echo 'PUBFIG83: DF, Variable hashing, 10% Known size, 90% Train/Test'
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 10 -ks 0.1 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 20 -ks 0.1 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 30 -ks 0.1 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 50 -ks 0.1 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -ks 0.1 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -ks 0.1 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -ks 0.1 -ts 0.9
 
-echo 'PUPFIG83: DF, Variable hashing, 50% Known size, 90% Train/Test'
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 10 -ks 0.5 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 20 -ks 0.5 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 30 -ks 0.5 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 50 -ks 0.5 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 100 -ks 0.5 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 300 -ks 0.5 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 500 -ks 0.5 -ts 0.9
+echo 'PUBFIG83: DF, Variable hashing, 50% Known size, 90% Train/Test'
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 10 -ks 0.5 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 20 -ks 0.5 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 30 -ks 0.5 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 50 -ks 0.5 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -ks 0.5 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -ks 0.5 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -ks 0.5 -ts 0.9
 
-echo 'PUPFIG83: DF, Variable hashing, 90% Known size, 90% Train/Test'
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 10 -ks 0.9 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 20 -ks 0.9 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 30 -ks 0.9 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 50 -ks 0.9 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 100 -ks 0.9 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 300 -ks 0.9 -ts 0.9
-python ../HFCN_openset_load.py -p ../features/ -f PUPFIG83-DEEP.bin -r 10 -m 500 -ks 0.9 -ts 0.9
+echo 'PUBFIG83: DF, Variable hashing, 90% Known size, 90% Train/Test'
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 10 -ks 0.9 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 20 -ks 0.9 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 30 -ks 0.9 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 50 -ks 0.9 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -ks 0.9 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -ks 0.9 -ts 0.9
+python ../HFCN_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -ks 0.9 -ts 0.9
