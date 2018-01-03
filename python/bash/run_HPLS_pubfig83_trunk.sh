c436cb9cae5a86449805cf51aa34223ed9f1a832
Rafael Vareto <rafaelvareto@gmail.com>
2018-01-03 19:01:57 -0200

    PLS: Adding trunking script for PubFig83

---
+++ python/bash/run_HPLS_pubfig83_trunk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 050 -t 0.0 -ks 0.1 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 100 -t 0.0 -ks 0.1 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 200 -t 0.0 -ks 0.1 -ts 0.5;
+
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 050 -t 0.0 -ks 0.5 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 100 -t 0.0 -ks 0.5 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 200 -t 0.0 -ks 0.5 -ts 0.5;
+
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 050 -t 1.0 -ks 0.1 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 100 -t 1.0 -ks 0.1 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 200 -t 1.0 -ks 0.1 -ts 0.5;
+
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 050 -t 1.0 -ks 0.5 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 100 -t 1.0 -ks 0.5 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 200 -t 1.0 -ks 0.5 -ts 0.5;
+
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 050 -t 2.0 -ks 0.1 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 100 -t 2.0 -ks 0.1 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 200 -t 2.0 -ks 0.1 -ts 0.5;
+
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 050 -t 2.0 -ks 0.5 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 100 -t 2.0 -ks 0.5 -ts 0.5;
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 5 -m 200 -t 2.0 -ks 0.5 -ts 0.5;
