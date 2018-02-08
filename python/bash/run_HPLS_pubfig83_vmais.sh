28554c8802086fdefaa0758bc75a9ffec504aa04
Rafael Vareto <rafaelvareto@gmail.com>
2018-02-08 16:23:13 -0200

    Updating vmais script

--- python/bash/run_HPLS_pubfig83_vmais.sh
+++ python/bash/run_HPLS_pubfig83_vmais.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
 #DF
-echo 'PUBFIG83: DF, Variable hashing, 20% Known size, 50% Train/Test'
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 010 -s 30 -ks 0.2 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 020 -s 30 -ks 0.2 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 030 -s 30 -ks 0.2 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 050 -s 30 -ks 0.2 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -s 30 -ks 0.2 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -s 30 -ks 0.2 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -s 30 -ks 0.2 -ts 0.5
+echo 'PUBFIG83: DF, Variable hashing, 10 Samples, 50% Known size, 50% Train/Test'
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 010 -s 10 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 020 -s 10 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 030 -s 10 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 050 -s 10 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -s 10 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -s 10 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -s 10 -ks 0.5 -ts 0.5
 
-echo 'PUBFIG83: DF, Variable hashing, 40% Known size, 50% Train/Test'
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 010 -s 40 -ks 0.4 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 020 -s 40 -ks 0.4 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 030 -s 40 -ks 0.4 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 050 -s 40 -ks 0.4 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -s 40 -ks 0.4 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -s 40 -ks 0.4 -ts 0.5
-python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -s 40 -ks 0.4 -ts 0.5
+echo 'PUBFIG83: DF, Variable hashing, 20 Samples, 50% Known size, 50% Train/Test'
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 010 -s 20 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 020 -s 20 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 030 -s 20 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 050 -s 20 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -s 20 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -s 20 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -s 20 -ks 0.5 -ts 0.5
+
+echo 'PUBFIG83: DF, Variable hashing, 30 Samples, 50% Known size, 50% Train/Test'
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 010 -s 30 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 020 -s 30 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 030 -s 30 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 050 -s 30 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -s 30 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -s 30 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -s 30 -ks 0.5 -ts 0.5
+
+echo 'PUBFIG83: DF, Variable hashing, 40 Samples, 50% Known size, 50% Train/Test'
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 010 -s 40 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 020 -s 40 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 030 -s 40 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 050 -s 40 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 100 -s 40 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 300 -s 40 -ks 0.5 -ts 0.5
+python ../HPLS_openset_load.py -p ../features/ -f PUBFIG83-DEEP.bin -r 10 -m 500 -s 40 -ks 0.5 -ts 0.5
